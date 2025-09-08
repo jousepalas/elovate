ae6b8cf655263a8d710d2dbf888e19616099a30e
Boss <jousepalas@hotmail.com>
2025-09-08 11:03:13 +0200

    added missing instalation files

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
 
-Route::apiResource('posts', ProductController::class);
+Route::apiResource('products', ProductController::class);
